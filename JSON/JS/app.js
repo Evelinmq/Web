946c7e853fd02d09d8f65097cf578c9a39355f9c
zurira <20243ds036@utez.edu.mx>
2025-10-20 13:00:48 -0600

    Ejercicio JSON

--- JSON/JS/app.js
+++ JSON/JS/app.js
@@ -1,7 +1,8 @@
 const persona = {
     nombre: "Evelin",
     edad: 20,
-    pais: "Mexico"
+    pais: "Mexico",
+    hobbies: ["Bailar, comer, dormir, chismear, cocinar, hornear, criticar hombres"]
 }; //Este es un objeto
 
 //Convertir onjeto a JSON (texto)
@@ -11,3 +12,9 @@ console.log(personaJSON);
 
 const resultado = document.querySelector(".resultado");
 resultado.textContent = personaJSON;
+
+resultado.innerHTML = 
+    "Nombre: " + persona.nombre + 
+    "<p> Edad: " + persona.edad +
+    "<p> Pais: " + persona.pais + 
+    "<p> Hobbies: " + persona.hobbies
