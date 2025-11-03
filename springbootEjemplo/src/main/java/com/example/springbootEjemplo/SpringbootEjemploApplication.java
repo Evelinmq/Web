96032e04dfcfe86bd3d54c54db8576875c03e2ad
Evelinmq <20243ds170utez.edu.mx>
2025-11-03 12:24:49 -0600

    sddf

--- springbootEjemplo/src/main/java/com/example/springbootEjemplo/SpringbootEjemploApplication.java
+++ springbootEjemplo/src/main/java/com/example/springbootEjemplo/SpringbootEjemploApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootEjemploApplication {
 
 	public static void main(String[] args) {
+        //ishdoif
 		SpringApplication.run(SpringbootEjemploApplication.class, args);
 	}
 
