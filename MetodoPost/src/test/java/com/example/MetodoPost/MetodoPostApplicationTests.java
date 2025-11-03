a8c920fe2f858b66eca315603cbba233716282f5
Evelinmq <20243ds170utez.edu.mx>
2025-11-03 13:52:24 -0600

    Ejemplo

---
+++ MetodoPost/src/test/java/com/example/MetodoPost/MetodoPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MetodoPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodoPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
