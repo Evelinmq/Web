9286fa73d1d04fabaa40c52edeafca35abe1b0f7
Evelinmq <20243ds170utez.edu.mx>
2025-11-03 12:25:14 -0600

    Sin comentario

--- springbootEjemplo/src/main/java/com/example/springbootEjemplo/SpringbootEjemploApplication.java
+++ springbootEjemplo/src/main/java/com/example/springbootEjemplo/SpringbootEjemploApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootEjemploApplication {
 
 	public static void main(String[] args) {
-        //ishdoif
 		SpringApplication.run(SpringbootEjemploApplication.class, args);
 	}
 
