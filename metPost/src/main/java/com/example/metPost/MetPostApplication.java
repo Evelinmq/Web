ed536ee4594477e8ea2565b1d25cb9f9f10298b7
Evelinmq <20243ds170@utez.edu.mx>
2025-11-09 13:57:28 -0600

    Trabajo

---
+++ metPost/src/main/java/com/example/metPost/MetPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.metPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetPostApplication.class, args);
+	}
+
+}
---
+++ metPost/src/test/java/com/example/metPost/MetPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
