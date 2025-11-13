a6f30b90e0b7ef998e6977f4249afa4bc3f6b9ed
Evelinmq <20243ds170@utez.edu.mx>
2025-11-13 14:25:54 -0600

    Trabajo

---
+++ FetchApplicationDos/src/main/java/com/example/FetchApplicationDos/FetchApplicationDosApplication.java
@@ -0,0 +1,13 @@
+package com.example.FetchApplicationDos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchApplicationDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchApplicationDosApplication.class, args);
+	}
+
+}
---
+++ FetchApplicationDos/src/main/java/com/example/FetchApplicationDos/HolaController.java
@@ -0,0 +1,22 @@
+package com.example.FetchApplicationDos;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+public class HolaController {
+    @PostMapping(
+            path = "/saludo",
+            //Enviar informaciòn en formato JSON
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            //Devuelve informaciòn en formato JSON
+            produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public Map<String, String> saludar (@RequestBody Map<String, String> datos) {
+        String nombre = datos.get("nombre");
+        String apellido = datos.get("apellido");
+        String mensaje = "Hola " + nombre + " " + apellido + " bienvenido al springboot y top tops";
+        return Map.of("mensaje", mensaje);
+    }
+}
